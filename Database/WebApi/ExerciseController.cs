2544e171e4e6e16c176d030fecc796ac53e632b7
A-Nastanliev <a.nastanliev@gmail.com>
2025-02-20 23:39:59 +0200

    Skibidi

---
+++ Database/WebApi/ExerciseController.cs
@@ -0,0 +1,85 @@
+﻿using DBContexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace WebApi
+{
+    [ApiController]
+    [Route("exercise/")]
+    public class ExerciseController : ControllerBase
+    {
+        private readonly ApiDbContext _dbContext;
+
+        public ExerciseController(ApiDbContext context)
+        {
+            _dbContext = context;
+            _dbContext.Database.EnsureCreated();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetExercise(int id)
+        {
+            try
+            {
+                Exercise exercise = await _dbContext.GetExercise(id);
+                if (exercise == null) return NotFound("User not found");
+                return Ok(exercise);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+        }
+
+
+        [HttpPost("{data}")]
+        public async Task<IActionResult> PostExercise([FromBody] Exercise data)
+        {
+            try
+            {
+                await _dbContext.AddExercise(data);
+
+                return Ok("User added successfully");
+            }
+            catch (DbUpdateException ex)
+            {
+                return BadRequest("Invalid user data.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+        }
+
+        [HttpPatch("{data}")]
+        public async Task<IActionResult> UpdateExercise(Exercise exercise)
+        {
+            try
+            {
+                await _dbContext.UpdateExercise(exercise);
+                return Ok();
+            }
+            catch (DbUpdateException ex)
+            {
+                return BadRequest("Couldn't update user data");
+            }
+        }
+
+        [HttpDelete("{email}")]
+        public async Task<IActionResult> DeleteExercise(int id)
+        {
+            try
+            {
+                await _dbContext.DeleteExercise(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ Database/WebApi/FoodController.cs
@@ -0,0 +1,85 @@
+﻿using DBContexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace WebApi
+{
+    [ApiController]
+    [Route("food/")]
+    public class FoodController : ControllerBase
+    {
+        private readonly ApiDbContext _dbContext;
+
+        public FoodController(ApiDbContext context)
+        {
+            _dbContext = context;
+            _dbContext.Database.EnsureCreated();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFood(int id)
+        {
+            try
+            {
+                Food food = await _dbContext.GetFood(id);
+                if (food == null) return NotFound("User not found");
+                return Ok(food);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+        }
+
+
+        [HttpPost("{data}")]
+        public async Task<IActionResult> PostFood([FromBody] Food data)
+        {
+            try
+            {
+                await _dbContext.AddFood(data);
+
+                return Ok("Food added successfully");
+            }
+            catch (DbUpdateException ex)
+            {
+                return BadRequest("Invalid food data.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+        }
+
+        [HttpPatch("{data}")]
+        public async Task<IActionResult> UpdateFood(Food food)
+        {
+            try
+            {
+                await _dbContext.UpdateFood(food);
+                return Ok();
+            }
+            catch (DbUpdateException ex)
+            {
+                return BadRequest("Couldn't update user data");
+            }
+        }
+
+        [HttpDelete("{email}")]
+        public async Task<IActionResult> DeleteFood(int id)
+        {
+            try
+            {
+                await _dbContext.DeleteFood(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
