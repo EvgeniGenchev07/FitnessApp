a948e2908e5f26d94c9fe4a2951ace5b8b1ff9e9
Ivan21T <119493817+Ivan21T@users.noreply.github.com>
2025-03-02 10:30:12 +0200

    small changes

--- Database/DBContexts/obj/Debug/net9.0/DBContexts.AssemblyInfo.cs
+++ Database/DBContexts/obj/Debug/net9.0/DBContexts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DBContexts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c0ec5c8173ebb93228c9fb38da56b43554a078c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16ba6b51e22b26ff265cdb129c1c12b649a269b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DBContexts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DBContexts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
