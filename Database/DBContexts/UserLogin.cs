cdee8067023d67fbacd9d57acf6f59d989a83189
Evgeni Genchev <120590298+EvgeniGenchev07@users.noreply.github.com>
2025-04-14 22:53:04 +0300

    bug fix
    login and register  functions fix

--- Database/DBContexts/UserLogin.cs
+++ Database/DBContexts/UserLogin.cs
@@ -21,7 +21,6 @@ public class UserLogin
         User user = await _userContext.ReadAsync(entity.Email, true);
         if (user == null) 
         {
-            entity.Password = BCrypt.Net.BCrypt.HashPassword(entity.Password);
             await _userContext.CreateAsync(entity);
             return (byte)Error.Ok;
         }
--- Database/Models/obj/Debug/net9.0/Models.AssemblyInfo.cs
+++ Database/Models/obj/Debug/net9.0/Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2e152e3e22f7254645ff4488a6a541ac0b35935")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d71b34f191646348e1f331a1f2e66d20b3c4bce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
