b0522941b2506b0553e15bafdd2f66cd33e30c64
Evgeni Genchev <120590298+EvgeniGenchev07@users.noreply.github.com>
2025-05-06 22:02:29 +0300

    commit

--- Database/DBContexts/obj/Debug/net9.0/DBContexts.AssemblyInfo.cs
+++ Database/DBContexts/obj/Debug/net9.0/DBContexts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DBContexts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d56d27e419108dcd87cf419a86bfc7b7634e46c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22e47817d32e0449a456f8ceac7dbb2f8f61f2a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DBContexts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DBContexts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
