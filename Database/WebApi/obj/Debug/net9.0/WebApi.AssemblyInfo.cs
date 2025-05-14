61ffd1633f734138a204bfcd275ff3fa6950ebd0
Evgeni Genchev <120590298+EvgeniGenchev07@users.noreply.github.com>
2025-05-14 19:30:12 +0300

    launchSettings.json added to the ignore file

--- Database/WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
+++ Database/WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc1d9b691a48496604124dbc296f60bcab514a91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ee631510b73d890a0c5761f0abafb77e8eb7996")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
